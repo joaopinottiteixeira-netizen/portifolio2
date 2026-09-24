2ef95a6c05940a53b34207964d4bcc1b23f26210
joaopinottiteixeira-netizen <joao.pinotti.teixeira@escola.pr.gov.br>
2026-09-24 09:44:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+  
+function calcular() {
+  nota1trim = Number(prompt("digita a notado primeiro triestre"));
+  nota2trim = Number(prompt("digita nota do segundo trimestre:"));
 
+   resultado = 180 - (nota1trim + nota2trim);
 
-resultado = 180 - (noa1trim + nota2rim);
-
-alert(resultado);
-
-
-
+  if(resultado <= 0){
+        alert("Parabéns!você esta aprovado.");
+    } else {
+        alert("você ainda precisa tirar " + resultado + " no terceiro trim");
+    }   
 }
